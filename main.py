68a020140dd83046cc6d13bf7429fd928efec88c
Oscar Belkadi <oscar.belkadi@etu.univ-amu.fr>
2022-12-08 22:40:24 +0100

    main.py update

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import NumPy as np
+import numpy as np
 import pygame as pg
+
+
+def main():
+
+    print("Hello World")
+
+class Complex:
+    
+    def __init__(self, a : int, b : int):
+        
+        """Create a new complex z = a + ib"""
+
+        self.a = a
+        self.b = b
+
+if __name__ == "__main__":
+    main()
