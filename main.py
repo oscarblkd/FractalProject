f83941c2a65ee0b6beee2c568786279627682437
Oscar Belkadi <oscar.belkadi@etu.univ-amu.fr>
2022-12-09 11:10:16 +0100

    Window init with pygame

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import numpy as np
-import pygame as pg
+import pygame, os
+
+os.environ["SDL_VIDEO_CENTERED"] = '1'  #for the fullscreen window
+
+
+WIDTH, HEIGHT = 1920, 1080
+window_size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(window_size)
 
 
 def main():
-
-    print("Hello World")
-
+    run = True
+    
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+    pygame.quit()
 class Complex:
     
     def __init__(self, a : int, b : int):
