9d3e95751c31fc816cf03eba36917537b3b5d5e8
Mottade <amarmatteo2003@gmail.com>
2022-12-09 19:13:40 +0100

    Oscar est un connard

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import pygame, os, complex
 
 
 
-
+#SETUP
 os.environ["SDL_VIDEO_CENTERED"] = '1'  #for the fullscreen window
 WIDTH, HEIGHT = 1920, 1080
 window_size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(window_size)
 BLUE = (13, 67, 216)
+
+#Objet complexe
+class Complex():
+
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def sum(self, unComplexe):
+        return self.real + unComplexe.real, self.imaginary + unComplexe.imaginary
+
+    
 
 def main():
     run = True
