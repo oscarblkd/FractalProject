cb7901b04c7ac46b424e2cafb607e9cfc7a30d20
Oscar Belkadi <oscar.belkadi@etu.univ-amu.fr>
2022-12-09 12:50:11 +0100

    method comprehension for changing pixels colors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pygame, os
 
@@ -7,17 +8,19 @@ os.environ["SDL_VIDEO_CENTERED"] = '1'  #for the fullscreen window
 WIDTH, HEIGHT = 1920, 1080
 window_size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(window_size)
-
+BLUE = (13, 67, 216)
 
 def main():
     run = True
-    
+    screen.fill(BLUE)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        pygame.display.update()
                 
     pygame.quit()
+
 class Complex:
     
     def __init__(self, a : int, b : int):
