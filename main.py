3a4397acc2dba6cdf4275f3a9156449c81dd131c
Oscar Belkadi <oscar.belkadi@etu.univ-amu.fr>
2022-12-19 16:57:05 +0100

    Julia function using cpu

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pygame, os
 from Complex import *
+from numba import jit, njit, vectorize, cuda, uint32, f8, uint8
 
 os.environ["SDL_VIDEO_CENTERED"] = '1'  #for the fullscreen window
 
@@ -11,11 +12,19 @@ screen = pygame.display.set_mode(window_size)
 
 BLUE = (13, 67, 216)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+C_JULIA_ONE = Complex(0.285, 0.01)
+C_JULIA_TWO = Complex(-0.8, 0.156)
+ZERO = Complex(0, 0)
+
+
+
 
 def main():
     run = True
-    screen.fill(BLUE)
-    
+    screen.fill(WHITE)
+    Julia(C_JULIA_TWO)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,8 +33,31 @@ def main():
                 
     pygame.quit()
 
-def Mandelbrot():
-    return 0
+def Julia(c = Complex):
+    
+    MAX_ITERATIONS = 5000
+    
+    real_min = -2
+    real_max = 2
+    imaginary_min = -2
+    imaginary_max = 2
+    
+    #Sequence of z(n + 1) = z(n)² + C where C is a constant 
+    
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            iterations = 0
+            z = Complex(real_min + (real_max - real_min) * x / WIDTH, 
+                        imaginary_min + (imaginary_max - imaginary_min) * y / HEIGHT)
+            
+            while(iterations < MAX_ITERATIONS) and Complex.abs(z) < 4:
+                iterations += 1
+                z = (z.product(z)).sum(c)
+                
+            #Color gestion 
+
+            color = int((16 * iterations / 100) % 255), int((16 * iterations / 100) % 255), int((128 * iterations / 100) % 255)
+            screen.set_at((x, y), color)
 
 if __name__ == "__main__":
     main()
